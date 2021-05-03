36b6f6a36d3af04bba87387630bf4e2b7a360b56
Reut-Maslansky <maslanskyr@gmail.com>
2021-05-03 14:38:40 +0300

    getColor

--- Color.hpp
+++ Color.hpp
@@ -11,24 +11,24 @@ namespace pandemic
         Black
     };
 
-    // void printColors(Color c)
-    // {
-    //     switch (c)
-    //     {
-    //     case Color::Red:
-    //         std::cout << "Red";
-    //         break;
-    //     case Color::Black:
-    //         std::cout << "Black";
-    //         break;
-    //     case Color::Blue:
-    //         std::cout << "Blue";
-    //         break;
-    //     case Color::Yellow:
-    //         std::cout << "Yellow";
-    //         break;
-    //     default:
-    //         break;
-    //     }
-    // }
+    static std::string getColors(Color c)
+    {
+        switch (c)
+        {
+        case Color::Red:
+            return "Red";
+            break;
+        case Color::Black:
+            return "Black";
+            break;
+        case Color::Blue:
+            return "Blue";
+            break;
+        case Color::Yellow:
+            return "Yellow";
+            break;
+        default:
+            break;
+        }
+    }
 }
